11f18a41814d02166b36cfccea047df070777a5d
Camelliaguoqian <3425296152@qq.com>
2019-11-19 19:48:03 +0800

    环境监测

--- src/views/EnvironmentalMonitoring/Index.vue
+++ src/views/EnvironmentalMonitoring/Index.vue
@@ -10,10 +10,11 @@
       <van-cell-group>
         <van-field 
           readonly
+          required
           clickable
-          label="管廊"
-          :value="pipe"
-          placeholder="请选择管廊"
+          label="所属管廊"
+          :value="pipeName"
+          placeholder="请选择所属管廊"
           @click="showPipePicker = true"
         />
         <van-popup v-model="showPipePicker" position="bottom">
@@ -27,11 +28,12 @@
 
         <van-field 
           readonly
+          required
           clickable
-          label="分区"
-          :value="zone"
-          placeholder="请选择分区"
-          @click="showZonePicker = true"
+          label="所属分区"
+          :value="zoneName"
+          placeholder="请选择所属分区"
+          @click="onClickZonePicker"
         />
         <van-popup v-model="showZonePicker" position="bottom">
           <van-picker 
@@ -44,11 +46,12 @@
 
         <van-field 
           readonly
+          required
           clickable
-          label="舱室"
-          :value="cabin"
-          placeholder="请选择舱室"
-          @click="showCabinPicker = true"
+          label="所属舱室"
+          :value="cabinName"
+          placeholder="请选择所属舱室"
+          @click="onClickCabinPicker"
         />
         <van-popup v-model="showCabinPicker" position="bottom">
           <van-picker 
@@ -58,7 +61,7 @@
             @confirm="onConfirmCabin"
           />
         </van-popup>
-
+    
       </van-cell-group>
     </div>
 
@@ -73,77 +76,199 @@
 </template>
 
 <script>
-import Vue from 'vue'
 import { NavBar,
  Cell, 
  CellGroup,  
- Icon, 
  Field,
- DatetimePicker,
  Picker,
  Popup,
- Toast,
  Button
 } from 'vant'
-
-Vue.use(NavBar)
-.use(Cell)
-.use(CellGroup)
-.use(Icon)
-.use(Field)
-.use(DatetimePicker)
-.use(Picker)
-.use(Popup)
-.use(Toast)
-.use(Button)
-
+import DataDictionaryUtil from 'utils/DataDictionaryUtil'
 
 export default {
   name: 'EnvironmentalMonitoringIndexPage',
+  components: {
+    [NavBar.name]: NavBar,
+    [Cell.name]: Cell,
+    [CellGroup.name]: CellGroup,
+    [Field.name]: Field,
+    [Picker.name]: Picker,
+    [Popup.name]: Popup,
+    [Button.name]: Button
+  },
   props: {
     zIndex: Number,
   },
   data() {
     return {
-      pipe: '',
+      pipeName: '',
+      pipeId: '',
       showPipePicker: false,
-      pipeColumns: ['黑龙江路综合管廊','习友路综合管廊','彩虹西路综合管廊','鸡鸣山路综合管廊'],
-      zone: '',
+      pipeColumns: [],
+      zoneName: '',
+      zoneId: '',
       showZonePicker: false,
-      zoneColumns: ['分区01','分区02','分区03'],
-      cabin: '',
+      zoneColumns: [],
+      cabinName: '',
+      cabinId: '',
       showCabinPicker: false,
-      cabinColumns: ['综合舱','燃气舱','电力舱','热力舱'], 
+      cabinColumns: [],
     }
   },
+  mounted: function() {
+    this.getPipeName();
+  },
   methods: {
-    goBack() {
+    goBack: function() {
       window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
     },
-    onConfirmPipe(pipe) {
-      this.pipe = pipe;
+    getPipeName: function() {
+      //管廊名称
+      this.request.httpPost(this.requestUrl.getPipe).then(data => {
+        let result = data; 
+        let resultRetCode = result.retCode; console.log(data);
+        let resultRetMsg = result.retMsg; 
+        let resultRetData = result.retData; 
+        
+        if(resultRetCode === "SUCCESS"){
+          //this.$toast(resultRetMsg);
+          for(let i=0; i<resultRetData.length; i++) {
+            this.pipeColumns.push(resultRetData[i].name);
+          }
+        }
+        if(resultRetCode === "FAIL"){
+          this.$toast(resultRetMsg);
+        } 
+      }).catch((error) => {
+        this.$toast("请求失败"+error);
+      });
+    },
+
+    initZonePicker(pid) {
+      //根据管廊ID查询分区接口
+      this.request.httpPost(this.requestUrl.getZoneByPipe, {
+        pipeId: pid}
+      ).then(data => {
+        let result = data; 
+        let resultRetCode = result.retCode; console.log(data);
+        let resultRetMsg = result.retMsg; 
+        let resultRetData = result.retData; 
+        
+        if(resultRetCode === "SUCCESS"){
+          //this.$toast(resultRetMsg);
+          for(let i=0; i<resultRetData.length; i++) {
+            this.zoneColumns.push(resultRetData[i].name);
+          }
+        }
+        if(resultRetCode === "FAIL"){
+          this.$toast(resultRetMsg);
+        } 
+      }).catch((error) => {
+        this.$toast("请求失败"+error);
+      });
+    },
+    initCabinPicker(pid,zid) {
+      //根据管廊ID、分区ID查询舱室接口
+      this.request.httpPost(this.requestUrl.getCabinByPipeZone, {
+        pipeId: pid,
+        zoneId: zid}
+      ).then(data => {
+        let result = data; 
+        let resultRetCode = result.retCode; console.log(data);
+        let resultRetMsg = result.retMsg; 
+        let resultRetData = result.retData; 
+        
+        if(resultRetCode === "SUCCESS"){
+          //this.$toast(resultRetMsg);
+          for(let i=0; i<resultRetData.length; i++) {
+            this.cabinColumns.push(resultRetData[i].name);
+          }
+        }
+        if(resultRetCode === "FAIL"){
+          this.$toast(resultRetMsg);
+        } 
+      }).catch((error) => {
+        this.$toast("请求失败"+error);
+      });
+    },
+
+
+    onConfirmPipe: function(pipeName) {
+      this.pipeName = pipeName;
+      switch(pipeName) {
+        case '黑龙江路综合管廊':
+          this.pipeId = '26';
+          break;
+      }
       this.showPipePicker = false;
+
+      //根据管廊ID查询分区接口
+      this.initZonePicker(this.pipeId);
     },
-    onConfirmZone(zone) {
-      this.zone = zone;
+    onClickZonePicker: function() {
+      //分区选择前先选择管廊
+      if(this.pipeName =='') {
+        this.$toast("请先选择所属管廊");
+      }else {
+        this.showZonePicker = true;
+      }
+    },
+    onConfirmZone: function(zoneName) {
+      this.zoneName = zoneName;
+      switch(zoneName) {
+        case '01#':
+          this.zoneId = '205';
+          break;
+      }
       this.showZonePicker = false;
+
+      //根据管廊ID查询分区接口
+      this.initCabinPicker(this.pipeId,this.zoneId);
     },
-    onConfirmCabin(cabin) {
-      this.cabin = cabin;
+    onClickCabinPicker: function() {
+      //舱室选择前先选择管廊和分区
+      if(this.pipeName =='') {
+        this.$toast("请先选择所属管廊");
+      }else if(this.zoneName =='') {
+        this.$toast("请先选择所属分区");
+      }else {
+        this.showCabinPicker = true;
+      }
+    },
+    onConfirmCabin: function(cabinName) {
+      this.cabinName = cabinName;
+      this.cabinId = '01';
       this.showCabinPicker = false;
     },
-    onSubmit() {
-      //console.log(this.$route); //通过 this.$route 访问当前路由
-      //通过 this.$router 访问路由器
-      this.$router.push('environmentalMonitoringList');
-
+   
+    onSubmit: function (e) {
+      e.preventDefault();
+      //校验
+      if(this.pipeId == ''
+      || this.zoneId == ''
+      || this.cabinId == ''){
+        this.$toast("请选择全部查询条件");
+        return false;
+      }else {
+        //带参数的跳转页面
+        this.$router.push({
+          path:"environmentalMonitoringList",
+          query: {
+            pipeId: this.pipeId,
+            zoneId: this.zoneId,
+            cabinId: this.cabinId,
+          }
+        })
+      }
     }
   }
 }
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .page {
+  padding: 46px 0 0 0;
   &-wrapper {
     padding: 0 10px;
     margin: 10px auto;
--- src/views/EnvironmentalMonitoring/List.vue
+++ src/views/EnvironmentalMonitoring/List.vue
@@ -6,32 +6,74 @@
     left-arrow 
     @click-left="goBack"></van-nav-bar>
 
-    
-    
+    <van-panel class="page-panel page-icon-panel">
+      <div class="panel-title van-hairline--bottom" slot="header">
+        <van-icon class="panel-title-icon" name="diamond" />
+        监测基本信息
+      </div>
+      <div class="page-panel-content">
+        <!-- 简单信息列表组件 -->
+        <simple-list :listdata="envlistdata"></simple-list>
+      </div>
+    </van-panel>
+
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
 import { NavBar,
  Panel,  
- Icon,
 } from 'vant'
-
-
-Vue.use(NavBar)
-.use(Panel)
-.use(Icon)
+import SimpleList from 'components/list/SimpleList'
 
 export default {
   name: 'EnvironmentalMonitoringListPage',
+  components: {
+    [NavBar.name]: NavBar,
+    [Panel.name]: Panel,
+    'simple-list': SimpleList
+  },
   props: {
     zIndex: Number,
   },
   data() {
     return {
-      
-    }
+      envlistdata: [
+        {
+          label: '所属管廊',
+          value: '黑龙江路综合管廊',
+        },
+        {
+          label: '所属分区',
+          value: '03分区',
+        },
+        {
+          label: '所属舱室',
+          value: '燃气舱',
+        },
+        {
+          label: '温度',
+          value: '65℃',
+        },
+        {
+          label: '湿度',
+          value: '26%RH',
+        },
+        {
+          label: '氧气含量',
+          value: '15%',
+        },
+        {
+          label: '甲烷含量',
+          value: '0.9%LEL',
+        },
+        {
+          label: '硫化氢含量',
+          value: '16mg/m3',
+        },
+        
+      ]
+    };
   },
   methods: {
     goBack() {
@@ -42,6 +84,54 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.page {
+  padding: 46px 0 0 0;
+  &-wrapper {
+    padding: 0 10px;
+    margin: 10px auto;
+  }
 
+  &-wrapper:last-child {
+    margin-bottom: 0;
+  }
+
+  &-panel {
+    margin: 10px auto;
+  }
+
+  &-panel:last-child {
+    margin-bottom: 0;
+  }
+
+  &-icon-panel {
+    &-content {
+      padding: 10px 16px;
+    }
+
+    .panel-title {
+      position: relative;
+      display: flex;
+      box-sizing: border-box;
+      width: 100%;
+      padding: 10px 16px;
+      overflow: hidden;
+      color: #323233;
+      font-size: 14px;
+      line-height: 24px;
+      background-color: #fff;
+      &-icon {
+        margin-right: 2px;
+        margin-top: 4px;
+      }
+      &-link {
+        margin-left: 10px;
+      }
+    }
+  }
+
+}
 </style>
+
+
+
 
